be9b098425e51a2cce851e7e7d0f107f7f841d2b
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2014-01-23 22:37:43 +0000

    Removed a bit of cruft

--- oscar_rosetta/dashboard/views.py
+++ oscar_rosetta/dashboard/views.py
@@ -1,37 +1,29 @@
 from rosetta import views
 
-from django.views.generic import TemplateView
+from django.views.generic import View
 from django.http import HttpResponse
 
 
-class IndexView(TemplateView):
-    template_name = 'rosetta/dashboard/index.html'
+class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        super(IndexView, self).get(request, *args, **kwargs)
         return_value = views.home(request)
-
         return return_value
 
     def post(self, request, *args, **kwargs):
         return_value = views.home(request)
-
         return return_value
 
 
-class LanguageSelectView(TemplateView):
-    template_name = 'rosetta/dashboard/index.html'
+class LanguageSelectView(View):
 
     def get(self, request, *args, **kwargs):
-        super(LanguageSelectView, self).get(request, *args, **kwargs)
         return_value = views.lang_sel(request, *args, **kwargs)
         return return_value
 
 
-class LanguagePickView(TemplateView):
-    template_name = 'rosetta/dashboard/index.html'
+class LanguagePickView(View):
 
     def get(self, request, *args, **kwargs):
-        super(LanguagePickView, self).get(request, *args, **kwargs)
         return_value = views.list_languages(request, *args, **kwargs)
         return return_value
